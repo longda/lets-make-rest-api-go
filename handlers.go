7d0a64c2e9edd49eae4ee6005d12289cedc1ab7e
David Long <longda@gmail.com>
2014-12-01 17:34:12 -0800

    Added mock db for gets.

--- handlers.go
+++ handlers.go
@@ -13,11 +13,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-  todos := Todos{
-    Todo{Name: "Write code"},
-    Todo{Name: "Host meetup"},
-  }
-
   w.Header().Set("Content-Type", "application/json; charset=UTF-8")
   w.WriteHeader(http.StatusOK)
   if err := json.NewEncoder(w).Encode(todos); err != nil {
