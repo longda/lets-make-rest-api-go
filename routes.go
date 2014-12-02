6a628cbbd2b55a6c8ff19d73cef098d8dc34d7c4
David Long <longda@gmail.com>
2014-12-01 17:20:44 -0800

    More refactoring.

--- routes.go
+++ routes.go
@@ -2,8 +2,6 @@ package main
 
 import (
   "net/http"
-
-  "github.com/gorilla/mux"
 )
 
 type Route struct {
@@ -14,23 +12,6 @@ type Route struct {
 }
 
 type Routes []Route
-
-func NewRouter() *mux.Router{
-  router := mux.NewRouter().StrictSlash(true)
-  for _, route := range routes {
-    var handler http.Handler
-    handler = route.HandlerFunc
-    handler = Logger(handler, route.Name)
-
-    router.
-      Methods(route.Method).
-      Path(route.Pattern).
-      Name(route.Name).
-      Handler(route.HandlerFunc)
-  }
-
-  return router;
-}
 
 var routes = Routes {
   Route {
