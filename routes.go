8b3a7d65d4df15aaeeb6cc1982119f2b3b95af92
David Long <longda@gmail.com>
2014-12-01 17:15:24 -0800

    Added diagnostic logging. Does not appear to work.

--- routes.go
+++ routes.go
@@ -18,6 +18,10 @@ type Routes []Route
 func NewRouter() *mux.Router{
   router := mux.NewRouter().StrictSlash(true)
   for _, route := range routes {
+    var handler http.Handler
+    handler = route.HandlerFunc
+    handler = Logger(handler, route.Name)
+
     router.
       Methods(route.Method).
       Path(route.Pattern).
