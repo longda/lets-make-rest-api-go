68704338883ea1cb0a621e98a0a70bf8177fe2b3
David Long <longda@gmail.com>
2014-12-01 16:29:27 -0800

    Basic routes.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "fmt"
-  "html"
   "log"
   "net/http"
 
@@ -12,10 +11,22 @@ import (
 func main() {
   router := mux.NewRouter().StrictSlash(true)
   router.HandleFunc("/", Index)
+  router.HandleFunc("/todos", TodoIndex)
+  router.HandleFunc("/todos/{todoId}", TodoShow)
 
   log.Fatal(http.ListenAndServe(":1337", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  todoId := vars["todoId"]
+  fmt.Fprintln(w, "Todo show:", todoId)
 }
